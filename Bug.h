d5185722803eee0ac1ad2c48a1edd29e3fa6c2fd
Bernard Joyce <joycebernie833@gmail.com>
2026-04-21 19:22:44 +0100

    implemented isWayBlocked method

--- Bug.h
+++ Bug.h
@@ -24,8 +24,20 @@ protected:
         bool alive {true};
         list<pair<int, int>> path;
         virtual void move() {}
-        bool isWayBlocked() {}
+        bool isWayBlocked() {
+                if (position.first==0 && direction_==west) {
+                        return true;
+                }
+                if (position.first==9 && direction_==east){
+                        return true;
+                }
+                if (position.second==0 && direction_==south) {
+                        return true;
+                }
+                if (position.second==9 && direction_==north) {
+                        return true;
+                }
+                return false;
+        }
 };
-
-
 #endif //A_BUGS_LIFE_BUG_H
---
+++ Crawler.h
@@ -0,0 +1,17 @@
+//
+// Created by Bernie Joyce on 21/04/2026.
+//
+
+#ifndef A_BUGS_LIFE_CRAWLER_H
+#define A_BUGS_LIFE_CRAWLER_H
+#include "Bug.h"
+
+
+class Crawler : Bug{
+        void move() {
+
+        }
+};
+
+
+#endif //A_BUGS_LIFE_CRAWLER_H
