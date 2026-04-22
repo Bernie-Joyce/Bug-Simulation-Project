d02d336a2a5ab084684f5abb3f371d3f1715461c
Bernard Joyce <joycebernie833@gmail.com>
2026-04-22 16:23:06 +0100

    added menu display

--- main.cpp
+++ main.cpp
@@ -10,6 +10,19 @@ int main() {
                 bug->display();
                 cout << endl;
         }
+
+
+        cout << "1. Initialize Bug Board (load data from file)" << endl;
+        cout << "2. Display all Bugs" << endl;
+        cout << "3. Find a Bug (given an id)" << endl;
+        cout << "4. Tap the Bug Board (causes move all, then fight/eat)" << endl;
+        cout << "5. Display Life History of all Bugs (path taken)" << endl;
+        cout << "6. Display all Cells listing their Bugs" << endl;
+        cout << "7. Run simulation (generates a Tap every second)" << endl;
+        cout << "8. Exit (write Life History of all Bugs to file)" << endl;
+        string command;
+        getline(cin,command);
+
         for (const Bug* bug: bug_vector) {
                delete bug;
         }
