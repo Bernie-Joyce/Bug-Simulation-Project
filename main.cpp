2866aed077dfe5da28f102d3687ef822c0f6ee1f
Bernard Joyce <joycebernie833@gmail.com>
2026-04-22 16:57:33 +0100

    adding defensive coding to the menu and a bugs loaded counter and changing where the user is asked for input

--- main.cpp
+++ main.cpp
@@ -14,51 +14,68 @@ int main() {
         cout << "7. Run simulation (generates a Tap every second)" << endl;
         cout << "8. Exit (write Life History of all Bugs to file)" << endl;
         string command;
-        getline(cin, command);
 
-        int commandNumber = stoi(command);
 
-        while (commandNumber != 0) {
+        int commandNumber = 0;
+
+        while (commandNumber != 8) {
+                cout << "Enter a command (1 - 8): ";
+                getline(cin, command);
+                try {
+                        commandNumber = stoi(command);
+                } catch (const std::exception&) {
+                        cout << "Please enter a valid number." << endl;
+                        continue;
+                }
+
                 switch (commandNumber) {
-                        case 1: FileReader::load("bugs.txt", bug_vector);
+                        case 1:
+                                if (bug_vector.empty()) {
+                                        FileReader::load("bugs.txt", bug_vector);
+                                        cout << "Bugs Loaded: " << bug_vector.size() << endl;
+                                } else {
+                                        cout << "Bugs already loaded" << endl;
+                                }
                                 break;
-                        case 2: for (Bug *bug: bug_vector) {
+                        case 2: if (bug_vector.empty()) cout << "No bugs loaded!" << endl;
+                                for (Bug *bug: bug_vector) {
                                         bug->display();
                                         cout << endl;
                                 }
                                 break;
                         case 3: {
-                                cout << "Input bug id" << endl;
-                                string bugId;
-                                cin.ignore();
-                                getline(cin, bugId);
-                                const int bugIdNum = stoi(bugId);
-                                bool found = false;
-                                for (Bug *bug: bug_vector) {
-                                        if (bug->getId() == bugIdNum) {
-                                                bug->display();
-                                                cout << endl;
-                                                found = true;
+                                try {
+                                        cout << "Input bug id" << endl;
+                                        string bugId;
+                                        getline(cin, bugId);
+                                        const int bugIdNum = stoi(bugId);
+                                        bool found = false;
+                                        for (Bug *bug: bug_vector) {
+                                                if (bug->getId() == bugIdNum) {
+                                                        bug->display();
+                                                        cout << endl;
+                                                        found = true;
+                                                }
+                                        }
+                                        if (!found) {
+                                                cout << "bug not found" << endl;
                                         }
                                 }
-                                if (!found) {
-                                        cout << "bug not found" << endl;
+                                catch (const std::exception&) {
+                                        cout << "Invalid Id" << endl;
                                 }
                                 break;
                         }
-
                         case 4:
                                 for (Bug *bug: bug_vector) {
                                         bug->move();
                                 }
                                 break;
+                        case 8:
+                                cout << "Program over";
+                                break;
                         default: cout << "Invalid command" << endl;
                 }
-                        cout << "Enter a new command: ";
-                        getline(cin, command);
-                        commandNumber = stoi(command);
-
-
         }
 
         for (const Bug *bug: bug_vector) {
